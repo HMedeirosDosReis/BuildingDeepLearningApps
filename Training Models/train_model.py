7d83bcd28f751418e74c96fc8ddd8240944d2b50
Henrique Medeiros Dos Reis <henriquereis20180@gmail.com>
2022-01-31 13:35:59 -0600

    finished creatingNeuralNetwork and started trainingModels

--- Training Models/train_model.py
+++ Training Models/train_model.py
@@ -1,6 +1,6 @@
 import pandas as pd
-from tensorflow.python.keras.models import Sequential
-from tensorflow.python.keras.layers import Dense
+from keras.models import Sequential
+from keras.layers import *
 
 training_data_df = pd.read_csv("sales_data_training_scaled.csv")
 
@@ -15,4 +15,14 @@ model.add(Dense(50, activation='relu'))
 model.add(Dense(1, activation='linear'))
 model.compile(loss='mean_squared_error', optimizer='adam')
 
+# Train the model
 
+
+# Load the separate test data set
+test_data_df = pd.read_csv("sales_data_test_scaled.csv")
+
+X_test = test_data_df.drop('total_earnings', axis=1).values
+Y_test = test_data_df[['total_earnings']].values
+
+test_error_rate =
+print("The mean squared error (MSE) for the test data set is: {}".format(test_error_rate))
